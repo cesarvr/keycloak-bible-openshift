601ab9e1003a10ef5a4e04a82634890df89a13c0
Cesar Valdez <cesarv01@yahoo.com>
2019-05-16 09:32:01 +0100

    Update export.sh

--- import-export/scripts/export.sh
+++ import-export/scripts/export.sh
@@ -6,6 +6,8 @@ echo #####################
 curl -o /opt/eap/standalone/configuration/standalone-openshift.xml https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/standalone-openshift.xml
 
 ## Modify configuation file using RHSSO container values
+source /opt/eap/bin/launch/openshift-common.sh
+source /opt/eap/bin/launch/logging.sh
 source /opt/eap/bin/launch/configure.sh
 
 ## Start RHSSO import mode
