ddb7b3e7ddd70d7e2444d525efc2899cbd1cf224
Cesar Valdez <cesarv01@yahoo.com>
2019-05-16 09:28:38 +0100

    Update export.sh

--- import-export/scripts/export.sh
+++ import-export/scripts/export.sh
@@ -2,9 +2,12 @@ echo #####################
 echo ## Starting Export ##
 echo #####################
 
-source /opt/eap/bin/launch/configure.sh
-
+## Fetch configuation file
 curl -o /opt/eap/standalone/configuration/standalone-openshift.xml https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/standalone-openshift.xml
 
+## Modify configuation file using RHSSO container values
+source /opt/eap/bin/launch/configure.sh
+
+## Start RHSSO import mode
 sh /opt/eap/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 -Dkeycloak.migration.action=export \
 -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/tmp/migrate.json
