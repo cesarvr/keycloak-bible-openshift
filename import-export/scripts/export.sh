34a5737e781e89aa090008667c1df814fdbd4e24
cesar <cesar.valdez@gosub.consulting>
2019-05-16 09:24:51 +0100

    improving rhsso export script

--- import-export/scripts/export.sh
+++ import-export/scripts/export.sh
@@ -3,5 +3,8 @@ echo ## Starting Export ##
 echo #####################
 
 source /opt/eap/bin/launch/configure.sh
+
+curl -o /opt/eap/standalone/configuration/standalone-openshift.xml https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/standalone-openshift.xml
+
 sh /opt/eap/bin/standalone.sh -c standalone-openshift.xml -bmanagement 127.0.0.1 -Dkeycloak.migration.action=export \
 -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/tmp/migrate.json
