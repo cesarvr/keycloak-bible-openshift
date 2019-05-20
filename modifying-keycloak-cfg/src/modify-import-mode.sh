55382837b05cff267af8e797327efc244f2706a0
cesar <cesar.valdez@gosub.consulting>
2019-05-20 13:30:19 +0100

    update

--- modifying-keycloak-cfg/src/modify-import-mode.sh
+++ modifying-keycloak-cfg/src/modify-import-mode.sh
@@ -3,10 +3,15 @@ main () {
  curl -o /opt/eap/standalone/configuration/standalone-openshift.xml https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/standalone-openshift.xml
 
  # copy modified rhsso launcher
- curl -o /opt/eap/standalone/bin/openshift-launch.sh https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/openshift-launch.sh
+ curl -o /opt/eap/bin/openshift-launch.sh https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/openshift-launch.sh
 
  # execute launcher
- sh /opt/eap/standalone/bin/openshift-launch.sh
+ FILE=/opt/eap/bin/openshift-launch.sh
+ if test -f "$FILE"; then
+    echo "$FILE exist"
+
+    sh $FILE
+ fi
 }
 
 main 
