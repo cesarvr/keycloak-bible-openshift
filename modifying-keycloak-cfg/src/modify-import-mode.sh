f261e15201803f5ea44b313e7ed4f0b86b2a98fe
cesar <cesar.valdez@gosub.consulting>
2019-05-20 13:20:21 +0100

    update

--- modifying-keycloak-cfg/src/modify-import-mode.sh
+++ modifying-keycloak-cfg/src/modify-import-mode.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+main () {
+ # copy fresh configuration file
+ curl -o /opt/eap/standalone/configuration/standalone-openshift.xml https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/standalone-openshift.xml
 
-# copy fresh configuration file
-curl -o /opt/eap/standalone/configuration/standalone-openshift.xml https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/standalone-openshift.xml
+ # copy modified rhsso launcher
+ curl -o /opt/eap/standalone/bin/openshift-launch.sh https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/openshift-launch.sh
 
-# copy modified rhsso launcher
-curl -o /opt/eap/standalone/bin/openshift-launch.sh https://raw.githubusercontent.com/cesarvr/keycloak-examples/master/modifying-keycloak-cfg/src/openshift-launch.sh
+ # execute launcher
+ sh /opt/eap/standalone/bin/openshift-launch.sh
+}
 
-# execute launcher
-#sh /opt/eap/standalone/bin/openshift-launch.sh
-
-sleep 3600
+main 
